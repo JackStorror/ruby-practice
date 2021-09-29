f9449e1922f95fc8b46fc4695aa2ffae7bbe2207
Jack Storror <jackstorror@gmail.com>
2021-09-29 11:52:31 +0100

    Update to current file versions

---
+++ new_game.rb
@@ -0,0 +1,8 @@
+charades.play
+    player_1.play
+    player_2.play
+
+    if three_rounds_played
+        break
+    end
+end
---
+++ while_loop.rb
@@ -0,0 +1,5 @@
+number_less_than_101 = 9
+while number_less_than_101 <= 99 do
+    number_less_than_101 = number_less_than_101 + 1
+    puts number_less_than_101
+end
