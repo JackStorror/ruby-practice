83d9cd11fefff0db77f0ffe1e13f7a17fed42d9c
Jack Storror <jackstorror@gmail.com>
2021-09-29 16:02:37 +0100

    Added some files

---
+++ different_objects_different_purposes.rb
@@ -0,0 +1,5 @@
+one_string = "1"
+two_string = "2"
+third_string = 3
+puts one_string + two_string
+puts one_string + third_string
---
+++ todo_dog.rb
@@ -0,0 +1,3 @@
+todo_1 = "wash the dog"
+todo_1.capitalize!
+puts todo_1
