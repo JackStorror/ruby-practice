49e0dbd9f6dd4e7485721f8b1c71d005d357f910
Jack Storror <jackstorror@gmail.com>
2021-09-29 11:58:58 +0100

    dice game added

---
+++ random_dice_game.rb
@@ -0,0 +1,9 @@
+player_1 = rand(5)
+player_2 = rand(5)
+    if player_1 > player_2
+        puts "Player 1 wins!"
+    elsif player_2 > player_1
+        puts "Player 2 wins!"
+    else
+        puts "It's a draw!"    
+    end 
